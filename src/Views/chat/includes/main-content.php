94ae2caa32b14c00e46f7e434ef934d3e33a267a
Oliver Bob R. Lagumen <oliverbob.lagumen@gmail.com>
2026-01-07 01:13:01 +0800

    Increase toast container z-index to 200 to show above modals

--- src/Views/chat/includes/main-content.php
+++ src/Views/chat/includes/main-content.php
@@ -197,4 +197,4 @@
 </main>
 
 <!-- Toast Notification Container -->
-<div id="toast-container" class="fixed bottom-4 right-4 z-[100] flex flex-col gap-2 pointer-events-none"></div>
+<div id="toast-container" class="fixed bottom-4 right-4 z-[200] flex flex-col gap-2 pointer-events-none"></div>
